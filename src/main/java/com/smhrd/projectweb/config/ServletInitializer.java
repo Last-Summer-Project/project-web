98156e8095b5a969ff9fde63a76a4ea3dc91d362
WonJung Kim <git@won-jung.kim>
2023-05-25 14:52:13 +0900

    fix(config): typo

--- src/main/java/com/smhrd/projectweb/config/ServletInitializer.java
+++ src/main/java/com/smhrd/projectweb/config/ServletInitializer.java
@@ -14,7 +14,7 @@ public class ServletInitializer extends AbstractAnnotationConfigDispatcherServle
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{DataSourceConfig.class, WebSecurityConfig.class, MethodSecuriyConfig.class};
+        return new Class<?>[]{DataSourceConfig.class, WebSecurityConfig.class, MethodSecurityConfig.class};
     }
 
     @Override
---
+++ src/main/java/com/smhrd/projectweb/service/TimelapseService.java
@@ -0,0 +1,4 @@
+package com.smhrd.projectweb.service;
+
+public class TimelapseService {
+}
