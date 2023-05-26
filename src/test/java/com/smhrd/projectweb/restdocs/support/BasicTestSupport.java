be35dffee57fd708d2ddf4c52f6066d93d9850f4
WonJung Kim <git@won-jung.kim>
2023-05-26 13:46:39 +0900

    feat(test): specify port and runner

--- src/test/java/com/smhrd/projectweb/restdocs/support/BasicTestSupport.java
+++ src/test/java/com/smhrd/projectweb/restdocs/support/BasicTestSupport.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.ApplicationContext;
@@ -15,6 +16,7 @@ import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -22,6 +24,7 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Disabled
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @Import(RestDocsConfig.class)
 @ExtendWith(RestDocumentationExtension.class)
